75ee8f7f850d2516cb363f710d64b4a6151aae1d
Muhamad Iqbal <37706944+mirfmaster@users.noreply.github.com>
2022-02-07 23:46:31 +0700

    update port to be more dynamic

--- src/core/boot.js
+++ src/core/boot.js
@@ -4,8 +4,8 @@ const ErrorHandler = require("../core/supports/ErrorHandler");
 kernel.use(routes);
 kernel.use(ErrorHandler);
 
-kernel.listen(3000, () => {
-  console.log(`App listening at port :3000`);
+kernel.listen(process.env.PORT || 3000, () => {
+  console.log(`App listening at port :${process.env.PORT || 3000}`);
 });
 
 module.exports = kernel;
